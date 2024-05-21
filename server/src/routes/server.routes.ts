db99cff39da013d338bd6fe8ecd747780cc1c101
iamabhas <abhas123mnm@gmail.com>
2024-05-21 15:38:08 +0545

    refactor: added get route and tested response handler

--- server/src/routes/server.routes.ts
+++ server/src/routes/server.routes.ts
@@ -1,6 +1,10 @@
-import { Application } from "express";
+import { Application,Response } from "express";
+import handleResponse from "../helpers/responseHandler";
 const initializeRoutes = (expressApplication: Application) => {
   console.log("Initialized Routes...!");
+  expressApplication.get("/", (_:any, res:Response) => {
+    handleResponse(res,200,"Git Automation")
+  })
 };
 
 export default initializeRoutes;
