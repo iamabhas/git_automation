c00fe5df56f5ef04e82ec446795ce4feb5cda0dc
iamabhas <abhas123mnm@gmail.com>
2024-05-22 13:06:47 +0545

    refactor: used github rest api route

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -7,15 +7,16 @@ class AuthService {
 
     public static async loginService(res: Response,body:any) {
             const {githubAccessToken} = body
+
             if(!githubAccessToken) {
                 throw new AppError("Github access token is missing !",400);
             }
+
             const octokit = new Octokit({auth:githubAccessToken});
 
-            const {data:{login}} = await octokit.rest.users.getAuthenticated()
+            const {data:user} = await octokit.request("GET /user")
 
-            const responseData = `Hello ${login}`
-            return responseHandler(res,200,"Github access token valid . Successfully logged in !",responseData)
+            return responseHandler(res,200,`Github access token valid . User ${user.login} successfully logged in !`)
     }
 
 
