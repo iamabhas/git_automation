944efc5af1b3888af8166dddcf1f156711060e4f
iamabhas <abhas123mnm@gmail.com>
2024-05-22 12:40:37 +0545

    refactor: added auth route and error handling middleware

--- server/src/routes/server.routes.ts
+++ server/src/routes/server.routes.ts
@@ -1,10 +1,32 @@
-import { Application,Response } from "express";
+import {Application, Response, Request, NextFunction} from "express";
 import handleResponse from "../helpers/responseHandler";
+import authRoutes from "./auth.routes";
+import handleError from "../helpers/errorHandler";
+import AppError from "../utils/appError.util";
+
+
 const initializeRoutes = (expressApplication: Application) => {
   console.log("Initialized Routes...!");
-  expressApplication.get("/", (_:any, res:Response) => {
-    handleResponse(res,200,"Git Automation")
+
+  // landing route
+  expressApplication.get("/", (req:Request,res:Response) => {
+    return handleResponse(res,200,"Git Automation")
   })
+
+  //app  routes
+  expressApplication.use("/api/",[
+      authRoutes
+  ])
+
+  // error route
+  expressApplication.all("*", (req:Request,res:Response,next:NextFunction) => {
+    next(new AppError(`Cannot find ${req.originalUrl}`,404))
+  })
+
+  expressApplication.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+    handleError(err, req, res, next);
+  });
+
 };
 
 export default initializeRoutes;
