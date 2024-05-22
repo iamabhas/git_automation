afe79f5184489c3f3f414f8bc53fa8c18b82435a
iamabhas <abhas123mnm@gmail.com>
2024-05-22 12:40:16 +0545

    refactor: changed formatting

--- server/src/middlewares/server.middleware.ts
+++ server/src/middlewares/server.middleware.ts
@@ -3,8 +3,8 @@ import cors from "cors";
 import morganMiddleware from "./morgan.middleware";
 
 const initializeMiddlewares = (expressApplication: Application) => {
-  expressApplication.use(cors());
   expressApplication.use(express.json());
+  expressApplication.use(cors());
   expressApplication.use(morganMiddleware);
   console.log("Initialized Middlewares...!");
 };
