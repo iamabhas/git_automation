5fd656d7c64fceb9dae6bdebaa632e8feaf0c8b4
iamabhas <abhas123mnm@gmail.com>
2024-05-22 13:36:23 +0545

    refactor: added gitRepository route

--- server/src/routes/server.routes.ts
+++ server/src/routes/server.routes.ts
@@ -1,25 +1,28 @@
 import {Application, Response, Request, NextFunction} from "express";
-import handleResponse from "../helpers/responseHandler";
-import authRoutes from "./auth.routes";
-import handleError from "../helpers/errorHandler";
 import AppError from "../utils/appError.util";
 
+import handleResponse from "../helpers/responseHandler";
+import handleError from "../helpers/errorHandler";
+
+import authRouter from "./auth.routes";
+import gitRepositoryRouter from "./gitRepository.routes";
 
 const initializeRoutes = (expressApplication: Application) => {
   console.log("Initialized Routes...!");
 
   // landing route
-  expressApplication.get("/", (req:Request,res:Response) => {
+  expressApplication.get("/", (_:Request,res:Response) => {
     return handleResponse(res,200,"Git Automation")
   })
 
   //app  routes
   expressApplication.use("/api/",[
-      authRoutes
+      authRouter,
+      gitRepositoryRouter
   ])
 
   // error route
-  expressApplication.all("*", (req:Request,res:Response,next:NextFunction) => {
+  expressApplication.all("*", (req:Request,_:Response,next:NextFunction) => {
     next(new AppError(`Cannot find ${req.originalUrl} on method ${req.method}`,404))
   })
 
