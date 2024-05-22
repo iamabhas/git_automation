e9be9d8585008709d9628f71c036c7ffcd3df5d6
iamabhas <abhas123mnm@gmail.com>
2024-05-22 13:34:34 +0545

    feat: added git repository controller

---
+++ server/src/controllers/gitRepository.controller.ts
@@ -0,0 +1,17 @@
+import {NextFunction,Request,Response} from "express";
+import GitRepositoryService from "../services/gitRepository.service";
+
+class GitRepositoryController {
+
+    public static async fetchAllReposController(req:Request,res:Response,next:NextFunction):Promise<any> {
+        try{
+            const body = req.body
+            await GitRepositoryService.fetchAllReposService(res,body)
+        }catch(err:any){
+            next(err)
+        }
+    }
+
+}
+
+export default GitRepositoryController
