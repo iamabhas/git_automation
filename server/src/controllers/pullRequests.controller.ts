b54ca21de3788b69db09ea6a6c190403786b349d
iamabhas <abhas123mnm@gmail.com>
2024-05-27 11:04:39 +0545

    feat: controller for prs

---
+++ server/src/controllers/pullRequests.controller.ts
@@ -0,0 +1,43 @@
+import {NextFunction,Request,Response} from "express";
+import PullRequestsService from "../services/pullRequests.service";
+
+class PullRequestsController {
+
+    public static async fetchPrsController(req:Request,res:Response,next:NextFunction):Promise<any> {
+        try{
+            const body = req.body
+            await PullRequestsService.fetchPrsService(res,body)
+        }catch(err:any){
+            next(err)
+        }
+    }
+
+    public static async fetchFilesFromPrController(req:Request,res:Response,next:NextFunction):Promise<any> {
+        try{
+            const body = req.body
+            await PullRequestsService.fetchFilesFromPrService(res,body)
+        }catch(err:any){
+            next(err)
+        }
+    }
+
+    public static async generateReviewForPrController(req:Request,res:Response,next:NextFunction):Promise<any> {
+        try{
+            const body = req.body
+            await PullRequestsService.generateReviewForPrService(res,body)
+        }catch(err:any){
+            next(err)
+        }
+    }
+
+    public static async createReviewCommentController(req:Request,res:Response,next:NextFunction):Promise<any> {
+        try{
+            const body = req.body
+            await PullRequestsService.createReviewCommentService(res,body)
+        }catch(err:any){
+            next(err)
+        }
+    }
+}
+
+export default PullRequestsController
