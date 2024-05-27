753ccb58b822bbea430214633f5f4cc1e75b389b
iamabhas <abhas123mnm@gmail.com>
2024-05-27 13:07:18 +0545

    refactor: used redux Provider component with store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import ThemeChanger from "./ThemeChanger.tsx";
+import {store} from "../src/redux/store.ts"
+import {Provider} from "react-redux";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <Provider store={store}>
       <ThemeChanger>
           <App />
       </ThemeChanger>
+      </Provider>
 
   </React.StrictMode>,
 )
