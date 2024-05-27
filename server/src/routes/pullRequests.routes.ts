08d6a8991a45a19e01cfd47c56939e4f7df4627a
iamabhas <abhas123mnm@gmail.com>
2024-05-27 11:05:01 +0545

    feat: Routes for prs

---
+++ server/src/routes/pullRequests.routes.ts
@@ -0,0 +1,21 @@
+import {Router} from "express";
+import PullRequestsController from "../controllers/pullRequests.controller";
+import validateToken from "../middlewares/tokenValidator.middleware";
+import {
+    fetchPrsValidation,
+    fetchFilesFromPrValidation,
+    generateReviewForPrValidation, createReviewCommentValidation
+} from "../validators/prs.validator";
+import {validateBody} from "../middlewares/validation.middleware";
+
+const pullRequestsRouter:Router = Router()
+
+pullRequestsRouter.get("/prs/fetch-prs",validateToken,fetchPrsValidation(),validateBody,PullRequestsController.fetchPrsController)
+
+pullRequestsRouter.get("/prs/fetch-pr-files",validateToken,fetchPrsValidation(),fetchFilesFromPrValidation(),validateBody,PullRequestsController.fetchFilesFromPrController)
+
+pullRequestsRouter.get("/prs/generate-review",validateToken,fetchPrsValidation(),fetchFilesFromPrValidation(),generateReviewForPrValidation(),validateBody,PullRequestsController.generateReviewForPrController)
+
+pullRequestsRouter.post("/prs/create-review-comment",validateToken,fetchPrsValidation(),fetchFilesFromPrValidation(),createReviewCommentValidation(),validateBody,PullRequestsController.createReviewCommentController)
+
+export default  pullRequestsRouter
