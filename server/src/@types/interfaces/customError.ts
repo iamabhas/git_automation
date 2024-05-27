a1f36099aae3238e1f1b37138d7ab6fcd1e9a3a3
iamabhas <abhas123mnm@gmail.com>
2024-05-27 11:00:21 +0545

    renamed these files

--- server/src/@types/interfaces/customError.ts
+++
@@ -1,5 +0,0 @@
-export interface ICustomError extends Error {
-    statusCode: number;
-    status: string;
-    message:string
-}
--- server/src/@types/interfaces/responseJson.ts
+++
@@ -1,7 +0,0 @@
-export interface IResponseJson {
-    statusCode:string
-    status:string,
-    message:string,
-    data?:any,
-    dataLength?:any
-}
