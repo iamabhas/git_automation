002cb554446bc242568d80150af8678ef6db5e9e
iamabhas <abhas123mnm@gmail.com>
2024-05-27 11:01:33 +0545

    refactor: changed formatting

--- server/src/middlewares/validation.middleware.ts
+++ server/src/middlewares/validation.middleware.ts
@@ -5,8 +5,11 @@ import responseHandler from "../helpers/responseHandler";
 export const validateBody = (req: Request, res: Response, next: NextFunction) => {
 
     const validationErrors = validationResult(req);
+
     if (validationErrors.isEmpty()) return next();
+
     const errors:[string] [] = [];
+
     validationErrors.array().map((err)=>{
         errors.push(err.msg)
     })
