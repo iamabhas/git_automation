4af1f95ad6acacc806a3e1c9451e38dc77ff984b
iamabhas <abhas123mnm@gmail.com>
2024-05-29 15:48:30 +0545

    feat: dashboard page

---
+++ client/src/pages/DashboardPage.tsx
@@ -0,0 +1,26 @@
+import DashboardComponent from "../components/appComponents/dashboardComponents/DashboardComponent.tsx";
+import {useSelector} from "react-redux";
+import {RootState} from "../redux/store.ts";
+
+const DashboardPage = () => {
+    const token = useSelector((state:RootState)=>state.auth.githubAccessToken)
+    console.log('This is token',token)
+    return (
+        <>
+            {token ? <div>
+                    <h1 className="flex flex-row  justify-center items-center text-4xl">
+                Welcome to Dashboard
+            </h1>
+                <div className="flex flex-row  justify-center items-center m-5">
+                <DashboardComponent/>
+                </div>
+            </div>:
+                <div className= "flex flex-row  justify-center items-center text-4xl">
+                401 | Unauthorized . Please Log in
+            </div>}
+        </>
+
+    );
+};
+
+export default DashboardPage;
