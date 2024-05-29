e86264907e9a6ab19916868bf422e7d310134165
iamabhas <abhas123mnm@gmail.com>
2024-05-29 15:45:07 +0545

    refactor: added redux persist logic

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,11 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
-import loginReducer from "./slices/auth.slice.ts"
+import authReducer from "./slices/auth.slice.ts"
+import storage from 'redux-persist/lib/storage'
+import {combineReducers} from "@reduxjs/toolkit";
+import {persistReducer,persistStore} from "redux-persist"
+
+const reducers = combineReducers({
+    auth:authReducer
+})
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig,reducers)
 
 export const store = configureStore({
-    reducer: {
-        counter: loginReducer,
-    },
+    reducer: persistedReducer
 })
+
+
+
+export const persistor=persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
