b8c18bc531d150c967867d13213c4a6902a0338c
iamabhas <abhas123mnm@gmail.com>
2024-05-29 15:44:16 +0545

    refactor: added redux persist for maintaing state

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import ThemeChanger from "./ThemeChanger.tsx";
-import {store} from "../src/redux/store.ts"
+import {store,persistor} from "../src/redux/store.ts"
 import {Provider} from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <Provider store={store}>
+          <PersistGate persistor={persistor}>
       <ThemeChanger>
           <App />
       </ThemeChanger>
+        </PersistGate>
       </Provider>
 
   </React.StrictMode>,
