cd41b59a2b7c7ffb79af80b131e191e1d9c90039
iamabhas <abhas123mnm@gmail.com>
2024-06-03 13:28:01 +0545

    refactor: added logout button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import LoginPage from './pages/LoginPage.tsx';
 import ErrorPage from "./pages/ErrorPage.tsx";
 import DashboardPage from "../src/pages/DashboardPage.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 const App = ()=> {
     return (
         <BrowserRouter>
-            <div className="text-right">
+            <div className="text-right flex flex-row justify-end">
                 <div className="m-1"><ModeToggle  /></div>
+                <Button className="m-1">Logout</Button>
             </div>
 
             <Routes>
