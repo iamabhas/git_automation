4a912e5452c2b0034a08c96664e73c91ed3c020b
iamabhas <abhas123mnm@gmail.com>
2024-06-03 13:29:54 +0545

    refactor: changed prompt for gemini

--- server/src/services/genAI.service.ts
+++ server/src/services/genAI.service.ts
@@ -22,7 +22,7 @@ class GenAIService {
     }
 
     public static reviewPrChanges = async (code:any)=>{
-        const prompt = `This prompt is being used while interacting with the google gemini api. So give me concise and clear answer to the following question because I need a short response. Give PR for following patch: ${code}. This should be short meaning just review and give what can be improved or what errors can be fixed. Also the answer should be in paragraph instead of points.`
+        const prompt = `This prompt is being used while interacting with the google gemini api. So give me concise and clear answer to the following question because I need a short response. Give PR for following patch: ${code}. This should be short meaning just review and give what can be improved or what errors can be fixed. Also the answer should be in paragraph instead of points. Don't start of by saying this "this patch includes this" or "patch  is this". If everything is ok say everything is okay and no need for changes . if code needs changes suggest changes and if there are error point out the error of how it should be done but dont give the code on how to fix it .just give what should be done in error thats it.Make the review comment short and sweet.`
         const response = await GenAIService.connectToModel().generateContent(prompt)
         const reply = response.response.text()
         return reply
